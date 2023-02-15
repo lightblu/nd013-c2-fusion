419666addff4f8d145a830926522486350718359
Lichtblau <b.lichtblau@fu-berlin.de>
2023-02-15 20:13:21 +0000

    Cleanup udacity repo files and update with latest workspace modifications, add gitignore and vmsetup script

---
+++ build/vmsetup.sh
@@ -0,0 +1,8 @@
+# Recurring tasks to do on a new vm
+git config --global user.email ""
+git config --global user.name ""
+git config --global credential.helper 'store --file ~/.git_credentials'
+git config --global push.default simple
+sudo apt update
+sudo apt install chromium-browser
+pip install pycodestyle seaborn
