ebf795d8a8765f89f02f3549f74837cc11052de5
Lichtblau, Bjoern (T3-CI) <bjoern.lichtblau@cariad.technology>
2023-03-13 11:30:03 +0100

    Fill in filter.py for step-1-final

--- student/filter.py
+++ student/filter.py
@@ -23,37 +23,96 @@ import misc.params as params
 
 class Filter:
     '''Kalman filter class'''
-    def __init__(self):
-        pass
+    def __init__(self, dt=params.dt, q=params.q):
+        self.dt = dt
+        self.q = q
 
     def F(self):
+        """
+        f is the state transition function state transition function (in the linear case, it is a matrix
+        F, also called system matrix system matrix). It tells us how to get from one timestamp to the next:
+        x[k] = f(x[k-1])+v = Fx[k-1] + v
+
+        1d case pos + v with t=1
+        return np.matrix([[1, 1],
+                        [0, 1]])
+        2d pos + v
+        return np.matrix([[1, 0, dt, 0],
+                          [0, 1, 0, dt],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]])
+        """
         ############
         # TODO Step 1: implement and return system matrix F
         ############
+        # 3d x + v
+        dt = self.dt
+        return np.matrix([[1, 0, 0, dt,  0,  0],
+                          [0, 1, 0,  0, dt,  0],
+                          [0, 0, 1,  0,  0, dt],
+                          [0, 0, 0,  1,  0,  0],
+                          [0, 0, 0,  0,  1,  0],
+                          [0, 0, 0,  0,  0,  1]])
 
-        return 0
         
         ############
         # END student code
         ############ 
 
     def Q(self):
+        """
+        ν∼N(0,Q) is the zero-mean process noise process noise with covariance Q
+
+        1d case:
+        return np.matrix([[0, 0],
+                          [0, 0]])
+
+        2d case:
+        return np.matrix([[q1, 0, q2, 0],
+                          [0, q1, 0, q2],
+                          [q2, 0, q3, 0],
+                          [0, q2, 0,  q3]])
+
+        Here q is a design parameter and should be chosen depending on the expected maximum change in velocity.
+        For highly dynamic maneuvers, we could use a higher process noise, e.g. 8 m/s2 would fit for emergency
+        braking, whereas for normal situations on a highway e.g. 3 m/s2 (==params.q) could be an adequate choice.
+        """
         ############
         # TODO Step 1: implement and return process noise covariance Q
         ############
+        q = self.dt * self.q
+        q2 = ((self.dt**2)/2) * self.q
+        q3 = ((self.dt**3)/3) * self.q
 
-        return 0
+        return np.matrix([[q3,  0,  0, q2,  0,  0],
+                          [ 0, q3,  0,  0, q2,  0],
+                          [ 0,  0, q3,  0,  0, q2],
+                          [q2,  0,  0,  q,  0,  0],
+                          [ 0, q2,  0,  0,  q,  0],
+                          [ 0,  0, q2,  0,  0,  q]])
         
         ############
         # END student code
         ############ 
+
 
     def predict(self, track):
         ############
         # TODO Step 1: predict state x and estimation error covariance P to next timestep, save x and P in track
         ############
 
-        pass
+        # Implement the F() and Q() functions to calculate a system matrix for constant velocity process model in 3D and the corresponding process noise covariance depending on the current timestep dt.
+        F = self.F()
+        x = F*track.x # predict the state
+        P = F*track.P*F.transpose() + self.Q() # predict the covariance
+
+        #  At the end of the prediction step, save the resulting x and P by calling the functions set_x() and set_P() that are already implemented in student/trackmanagement.py.
+        # x is the  state vector. It contains information about position and velocity of the object that you are tracking: (px, py, pz, vx, vy, vz)
+        # (ego coordinate system: x forward, y right, z up)
+        track.set_x(x)
+        # P is the estimation error covariance matrix estimation error covariance matrix, which contains information about
+        # the uncertainty of the object's position and velocity.
+        track.set_P(P)
         
         ############
         # END student code
@@ -63,7 +122,13 @@ class Filter:
         ############
         # TODO Step 1: update state x and covariance P with associated measurement, save x and P in track
         ############
-        
+        H = meas.sensor.get_H(track.x)  # measurement matrix
+        K = track.P*H.transpose()*np.linalg.inv(self.S(track, meas, H)) # Kalman gain
+        x = track.x + K * self.gamma(track, meas) # state update
+        P = (np.identity(params.dim_state) - K*H) * track.P # covariance update
+        track.set_x(x)
+        track.set_P(P)
+
         ############
         # END student code
         ############ 
@@ -73,8 +138,7 @@ class Filter:
         ############
         # TODO Step 1: calculate and return residual gamma
         ############
-
-        return 0
+        return meas.z - meas.sensor.get_hx(track.x)
         
         ############
         # END student code
@@ -84,8 +148,7 @@ class Filter:
         ############
         # TODO Step 1: calculate and return covariance of residual S
         ############
-
-        return 0
+        return (H * track.P * H.transpose()) + meas.R # covirance of residual
         
         ############
         # END student code
